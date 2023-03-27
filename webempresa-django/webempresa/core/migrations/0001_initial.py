01abbaf452fdcb308f5337bd3015d8cb30db1778
Carlos <cdmejia4@utpl.edu.ec>
2023-03-26 23:19:20 -0500

    subida media

---
+++ webempresa-django/webempresa/core/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Service',
+            fields=[
+                ('title', models.CharField(max_length=200, verbose_name='Título')),
+                ('subtitle', models.CharField(max_length=200, verbose_name='Subtítulo')),
+                ('content', models.TextField(verbose_name='Contenido')),
+                ('image', models.ImageField(upload_to='services', verbose_name='Imagen')),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')),
+                ('updated', models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')),
+            ],
+            options={
+                'verbose_name': 'servicio',
+                'verbose_name_plural': 'servicios',
+                'ordering': ['-created'],
+            },
+        ),
+    ]
